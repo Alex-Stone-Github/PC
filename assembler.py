de8c7325c9a92e8c5ecb1a8ae8b46e9b1a604717
Alex Stone <Stonealex145@gmail.com>
2021-11-14 16:24:23 +0000

    Finished emulator

---
+++ assembler.py
@@ -0,0 +1,25 @@
+# typedef struct Instruction {
+#     byte data;
+#     byte processorMode;
+#     bool COND;
+
+#     bool RA;
+#     bool RB;
+#     bool RC;
+#     bool RD;
+#     bool RR;
+#     bool RID;
+#     bool RP;
+
+#     bool WA;
+#     bool WB;
+#     bool WC;
+#     bool WD;
+#     bool WPC;
+#     bool WRA;
+#     bool WR;
+#     bool WOD;
+#     bool WPA;
+#     bool WPB;
+# } Instruction;
+
