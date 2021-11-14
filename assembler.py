bc34f31027ee89baf4ac263eb26a544e3bd2f2df
Alex Stone <Stonealex145@gmail.com>
2021-11-14 16:34:08 +0000

    Defined data types for assembler

--- assembler.py
+++ assembler.py
@@ -23,3 +23,35 @@
 #     bool WPB;
 # } Instruction;
 
+class instruction:
+    def __init__(self):
+        self.data = 0
+        self.mode = 0
+        self.COND = 0
+
+        self.RA = 0
+        self.RB = 0
+        self.RC = 0
+        self.RD = 0
+        self.RR = 0
+        self.RID = 0
+        self.RP = 0
+
+        self.WA = 0
+        self.wB = 0
+        self.WC = 0
+        self.WD = 0
+        self.WPC = 0
+        self.WRA = 0
+        self.WR = 0
+        self.WOD = 0
+        self.WPA = 0
+        self.WPB = 0
+    
+    def __str__(self):
+        f1="{"
+        f2=f"{self.data}, {self.mode}, {self.COND}"
+        f3=f"{self.RA}, {self.RB}, {self.RC}, {self.RD}, {self.RR}, {self.RID}, {self.RP}"
+        f4=f"{self.WA}, {self.WB}, {self.WC}, {self.WD}, {self.WPC}, {self.WRA}, {self.WR}, {self.WOD}, {self.WPA}, {self.WPB}"
+        f5="}"
+        return f1 + f2 + f3 + f4 + f5
